3845f34d99dbf4cceb8a8f2399269c42c5c6a024
SHADAB AHMAD <sa8377@srmist.edu.in>
2026-02-16 22:33:24 +0530

    Normal Printing OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,8 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        
+        // Printing the literal text "OOPS" to console
+        System.out.println("OOPS");
+        
+    }
+}
